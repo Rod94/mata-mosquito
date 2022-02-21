fae60906b7dd8617f2a14bdcce8784ded54c123b
Rodolpho Caetano <rphfti@hotmail.com>
2022-02-21 18:34:45 -0300

    colocando mosquito na tela

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ var largura = 0;
 function ajustaTamanhoJogo() {
   altura = window.innerHeight;
   largura = window.innerWidth;
-  console.log(altura, largura)
+  console.log(altura, largura);
 }
 
-ajustaTamanhoJogo()
+ajustaTamanhoJogo();
+
+
+var posicaoX = Math.floor(Math.random() * largura);
+var posicaoY = Math.floor(Math.random() * altura);
