bff33c77dba4e242c34212c795a63a95ebe697e7
Rodolpho Caetano <rphfti@hotmail.com>
2022-02-21 18:28:48 -0300

    criando arquivos html e js

---
+++ app.js
@@ -0,0 +1,10 @@
+var altura = 0;
+var largura = 0;
+
+function ajustaTamanhoJogo() {
+  altura = window.innerHeight;
+  largura = window.innerWidth;
+  console.log(altura, largura)
+}
+
+ajustaTamanhoJogo()
