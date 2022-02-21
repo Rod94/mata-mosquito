623c159d6504d994bddfa40ebe53649a8b5ca1b0
Rodolpho Caetano <rphfti@hotmail.com>
2022-02-21 18:51:14 -0300

    criada function para o mosquito randomizar

--- app.js
+++ app.js
@@ -4,11 +4,25 @@ var largura = 0;
 function ajustaTamanhoJogo() {
   altura = window.innerHeight;
   largura = window.innerWidth;
-  console.log(altura, largura);
 }
 
 ajustaTamanhoJogo();
 
+function posicaoRandomica() {
+  var posicaoX = Math.floor(Math.random() * largura) - 90;
+  var posicaoY = Math.floor(Math.random() * altura) - 90;
 
-var posicaoX = Math.floor(Math.random() * largura);
-var posicaoY = Math.floor(Math.random() * altura);
+  posicaoX = posicaoX < 0 ? 0 : posicaoX;
+  posicaoY = posicaoY < 0 ? 0 : posicaoY;
+
+  //criando o elemento html
+  var mosquito = document.createElement("img"); //criando uma imagem
+  mosquito.src = 'imagens/mosquito.png'; //src da imagem
+  mosquito.className = 'mosquito'; //class da imagem
+  mosquito.style.left = posicaoX + 'px';
+  mosquito.style.top = posicaoY + 'px';
+  mosquito.style.position = 'absolute';
+
+  document.body.appendChild(mosquito); //adicionando um filho para o body
+}
+
