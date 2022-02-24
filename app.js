d572db0cd82126eb8c7c38131e52ec8e5a693630
Rodolpho Caetano <rphfti@hotmail.com>
2022-02-24 18:22:47 -0300

    tela de vitoria adicionada

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var cronometro = setInterval(function(){
   if (tempo < 0) {
     clearInterval(cronometro);//limpar da memoria
     clearInterval(criaMosquito);
-    alert('vitoria');
+    window.location.href = 'vitoria.html'
   } else {
     document.getElementById('crono').innerHTML = tempo;
   }  
